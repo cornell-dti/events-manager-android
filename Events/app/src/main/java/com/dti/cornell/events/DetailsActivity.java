1c349cf6f496a3cdf9fd6651566efd80fe1fe267
JBoss925 <jaggerbrulato@gmail.com>
2018-02-27 17:43:04 -0500

    Updated w/ facelift

---
+++ Events/app/src/main/java/com/dti/cornell/events/DetailsActivity.java
@@ -0,0 +1,75 @@
+package com.dti.cornell.events;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.support.v7.widget.Toolbar;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * Created by jaggerbrulato on 2/27/18.
+ */
+
+public class DetailsActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private Event event = new Event();
+
+    private static final int MAP_ZOOM = 16;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void findViews(){
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Loads {@link Event#latitude} and longitude onto the map, adds a marker.
+     *
+     * @param map {@inheritDoc}
+     */
+    @Override
+    public void onMapReady(GoogleMap map)
+    {
+        LatLng position = new LatLng(event.latitude, event.longitude);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(position, MAP_ZOOM));
+        map.addMarker(new MarkerOptions().position(position).title(event.caption));
+    }
+
+}
---
+++ Events/app/src/main/java/com/dti/cornell/events/Event.java
@@ -0,0 +1,14 @@
+package com.dti.cornell.events;
+
+/**
+ * Created by jaggerbrulato on 2/27/18.
+ */
+
+public class Event {
+
+    public double latitude = 23;
+    public double longitude = 26;
+    public String caption = "ya";
+
+
+}
