46e42a3077c9c1352cb5fc473685d3018767a5c6
JBoss925 <jaggerbrulato@gmail.com>
2018-03-06 15:17:44 -0500

    Implemented a preliminary tag sliding system. General cleanup and listener handling.

--- Events/app/src/main/java/com/dti/cornell/events/DetailsActivity.java
+++ Events/app/src/main/java/com/dti/cornell/events/DetailsActivity.java
@@ -1,11 +1,16 @@
 package com.dti.cornell.events;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.support.v7.widget.Toolbar;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,11 +23,16 @@ import com.google.android.gms.maps.model.MarkerOptions;
  * Created by jaggerbrulato on 2/27/18.
  */
 
-public class DetailsActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class DetailsActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnTouchListener{
 
     private Event event = new Event();
 
     private static final int MAP_ZOOM = 16;
+
+    ConstraintLayout _view;
+    ConstraintLayout _root;
+    private int _xDelta;
+    private float tagX;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,18 @@ public class DetailsActivity extends AppCompatActivity implements OnMapReadyCall
         setContentView(R.layout.activity_details);
 //        Toolbar toolbar = findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
+        _view = findViewById(R.id.tagsLayout);
+        this.tagX = _view.getX();
+
+//        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 50);
+//        layoutParams.leftMargin = 50;
+//        layoutParams.topMargin = 50;
+//        layoutParams.bottomMargin = -250;
+//        layoutParams.rightMargin = -250;
+//        _view.setLayoutParams(layoutParams);
+
+        _view.setOnTouchListener(this);
+        _root = findViewById(R.id.tagsLayout);
     }
 
     @Override
@@ -70,6 +92,32 @@ public class DetailsActivity extends AppCompatActivity implements OnMapReadyCall
         LatLng position = new LatLng(event.latitude, event.longitude);
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(position, MAP_ZOOM));
         map.addMarker(new MarkerOptions().position(position).title(event.caption));
+    }
+
+
+    public boolean onTouch(View view, MotionEvent event) {
+        final int X = (int) event.getRawX();
+        final int Y = (int) event.getRawY();
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                ConstraintLayout.LayoutParams lParams = (ConstraintLayout.LayoutParams) view.getLayoutParams();
+                _xDelta = X - lParams.leftMargin;
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) view.getLayoutParams();
+                layoutParams.leftMargin = (int)tagX + (X - _xDelta);
+                view.setLayoutParams(layoutParams);
+//                int move = X - (_xDelta / 50);
+//                _root.setX(tagX + move);
+                break;
+        }
+        return true;
     }
 
 }
