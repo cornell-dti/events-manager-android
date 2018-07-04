a713c5b68b865ee90c8f5a46ffb6212c14f0c058
David Chu <davidchuyaya@gmail.com>
2018-07-04 15:10:22 -0400

    Added toolbar with correct style

--- Events/app/src/main/java/com/dti/cornell/events/MainActivity.java
+++ Events/app/src/main/java/com/dti/cornell/events/MainActivity.java
@@ -6,18 +6,24 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import net.danlew.android.joda.JodaTimeAndroid;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener
 {
+	private Toolbar toolbar;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		JodaTimeAndroid.init(this);
+
+		toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
 
 		BottomNavigationView tabBar = findViewById(R.id.tabBar);
 		tabBar.setOnNavigationItemSelectedListener(this);
@@ -31,15 +37,19 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 		switch (item.getItemId())
 		{
 			case R.id.tab_discover:
+				toolbar.setTitle(R.string.tab_discover);
 				fragment = new DiscoverFragment();
 				break;
 			case R.id.tab_for_you:
+				toolbar.setTitle(R.string.tab_for_you);
 				fragment = new ForYouFragment();
 				break;
 			case R.id.tab_my_events:
+				toolbar.setTitle(R.string.tab_my_events);
 				fragment = new MyEventsFragment();
 				break;
 			case R.id.tab_profile:
+				toolbar.setTitle(R.string.tab_profile);
 				fragment = new ProfileFragment();
 				break;
 			default:
