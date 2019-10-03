6b0288fde2b8a83ce223f6030748dc54de8d087f
Jagger Brulato <jb2375@cornell.edu>
2019-10-03 06:36:06 -0400

    ForYou fragment now updates on Data updates as well as fragment changes.

--- Events/app/src/main/java/com/dti/cornell/events/ForYouFragment.java
+++ Events/app/src/main/java/com/dti/cornell/events/ForYouFragment.java
@@ -6,12 +6,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.dti.cornell.events.models.CardList;
+import com.dti.cornell.events.models.Event;
+import com.dti.cornell.events.models.Organization;
+import com.dti.cornell.events.utils.Comparators;
 import com.dti.cornell.events.utils.Data;
 import com.dti.cornell.events.utils.EventBusUtils;
 import com.dti.cornell.events.utils.RecyclerUtil;
 import com.dti.cornell.events.utils.TagUtil;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import androidx.annotation.NonNull;
@@ -19,9 +23,10 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ForYouFragment extends Fragment
+public class ForYouFragment extends Fragment implements Data.DataUpdateListener
 {
 	public RecyclerView recyclerView;
+	public ViewGroup container;
 	private int mTotalScrolled;
 
 	@Nullable
@@ -29,26 +34,34 @@ public class ForYouFragment extends Fragment
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
 	{
 		View view = inflater.inflate(R.layout.fragment_recycler, container, false);
+		Data.registerListener(this);
 
 		RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+		this.recyclerView = recyclerView;
+		this.container = container;
+
+		populate();
+		RecyclerUtil.addVerticalSpacing(recyclerView);
+		setOnScrollListener();
+
+		return view;
+	}
+
+	public void populate(){
 		List<Integer> bestTagIDs = TagUtil.getMostPopularTags(3);
 		List<CardList> data = new ArrayList<>();
 		for(int tagID : bestTagIDs){
-			data.add(new CardList(Data.tagForID.get(tagID), true, TagUtil.suggestEventsForTagID(tagID)));
+			List<Event> events = TagUtil.suggestEventsForTagID(tagID);
+			events.sort(Comparators.NUM_ATTENDEES);
+			Collections.reverse(events);
+			data.add(new CardList(Data.tagForID.get(tagID), true, events));
 		}
 		if(data.size() == 0){
 			container.findViewById(R.id.noEventsForYouLayout).setVisibility(View.VISIBLE);
 		} else {
 			container.findViewById(R.id.noEventsForYouLayout).setVisibility(View.GONE);
 		}
-		RecyclerUtil.addVerticalSpacing(recyclerView);
 		recyclerView.setAdapter(new CardSectionAdapter(recyclerView.getContext(), data, false));
-
-		this.recyclerView = recyclerView;
-
-		setOnScrollListener();
-
-		return view;
 	}
 
 	public void setOnScrollListener(){
@@ -62,5 +75,20 @@ public class ForYouFragment extends Fragment
 				EventBusUtils.SINGLETON.post(new EventBusUtils.MainActivityScrolled(mTotalScrolled));
 			}
 		});
+	}
+
+	@Override
+	public void eventUpdate(List<Event> e) {
+		populate();
+	}
+
+	@Override
+	public void orgUpdate(List<Organization> o) {
+
+	}
+
+	@Override
+	public void tagUpdate(List<String> t) {
+
 	}
 }
